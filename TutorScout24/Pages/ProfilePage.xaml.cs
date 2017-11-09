0eff64e989315063df76e4b3d7bfa9c2e94348fc
Alexander <Alexander@ITDEWHMM76490>
2017-11-09 09:04:39 +0100

    Added Profile and Tutorial Page

---
+++ TutorScout24/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using TutorScout24.Controls;
+using TutorScout24.ViewModels;
+using Xamarin.Forms;
+
+namespace TutorScout24
+{
+    public partial class ProfilePage
+    {
+        public ProfilePage()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+    }
+}
